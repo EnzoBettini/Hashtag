361639ef18bb1b6fbc6e070141f773550c8f4e4b
Enzo Bettini <enzo.bettini@coopertec.com.br>
2025-07-23 13:48:48 -0300

    Finalização aulas 23/07/2025

--- Projetos/Projeto 1/main.py
+++ Projetos/Projeto 1/main.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+lista_usuarios = ['enzo', 'carlos', 'isabela']
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template('home.html')
+
+@app.route("/contato")
+def contato():
+    return render_template('contato.html')
+
+@app.route("/usuarios")
+def usuarios():
+    return render_template('usuarios.html', lista_usuarios=lista_usuarios)
+
+if __name__  == '__main__':
+    app.run(debug=True)
