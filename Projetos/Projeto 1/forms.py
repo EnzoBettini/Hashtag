203b088a5f99f62f0c161c039da2b3dce2dc77e4
Enzo Bettini <enzo.bettini@coopertec.com.br>
2025-07-24 13:59:00 -0300

    Finalização aulas 24/07/2025

---
+++ Projetos/Projeto 1/forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class FormCriarConta(FlaskForm):
+    username = StringField('Nome de Usuário', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    confirmacao_senha = PasswordField('Confirmar Senha', validators=[DataRequired(), EqualTo('senha')])
+    botao_submit_criar_conta = SubmitField('Criar Conta')
+
+class FormLogin(FlaskForm):
+    email = StringField('Nome de Usuário', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha')
+    botao_submit_login = SubmitField('Fazer Login')
--- Projetos/Projeto 1/main.py
+++ Projetos/Projeto 1/main.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from forms import FormCriarConta, FormLogin
 
 app = Flask(__name__)
 
 lista_usuarios = ['enzo', 'carlos', 'isabela']
+
+app.config['SECRET_KEY'] = 'd61fc15acb695169f7179d578ed67cf7'
+
 
 @app.route("/")
 def home():
@@ -15,6 +19,12 @@ def contato():
 @app.route("/usuarios")
 def usuarios():
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
+
+@app.route("/login")
+def login():
+    form_criar = FormCriarConta()
+    form_login = FormLogin()
+    return render_template('login.html', FormCriarConta=form_criar, FormLogin=form_login)
 
 if __name__  == '__main__':
     app.run(debug=True)
